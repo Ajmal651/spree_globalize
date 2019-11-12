074649691b851a0720127df39914b3966f02cff0
Ajmal-hassan <ajmalhassan651@gmail.com>
2019-11-12 17:56:18 +0500

    front-end js file added

--- lib/generators/spree_globalize/install/install_generator.rb
+++ lib/generators/spree_globalize/install/install_generator.rb
@@ -5,6 +5,7 @@ module SpreeGlobalize
 
       def add_javascripts
         append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_globalize\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_globalize\n"
 			end
 
       def add_stylesheets
